c563d4d3cada34b7e8dc967d55413bb5d15da2fe
TomtheLegend <38764023+TomtheLegend@users.noreply.github.com>
2026-06-15 21:46:00 +1200

    Complete bot 1.0 version
    adds in cube selection and cube list options to the bot.

--- main.py
+++ main.py
@@ -24,11 +24,37 @@ scheduled_time = [
     # datetime.time(hour=17, minute=0, tzinfo=nz_tz)  
 ]
 
-                  
 @commands.command()
-async def p1p1( ctx):
+async def cubelist(ctx):
+    embed = discord.Embed(
+            title="Cubes List", 
+            description="Current Cube list to request p1p1 from.\nUse the alias after p1p1 command to request", 
+            color=discord.Color.green()
+        )
+
+    all_cubes = get_cube_lsit()
+
+    cube_names = ""
+    cube_aliases = ""
+    cube_owners = ""
+
+    for cube in all_cubes:
+        cube_names += cube["cube_name"] + "\n"
+        cube_aliases += cube["cube_alias"] + "\n"
+        # hack to mkae it fit in an embed (limited width)
+        padding = (22 - len(cube["cube_owner_name"])) * " \u200b"
+        cube_owners += cube["cube_owner_name"] + padding + "[Link](" + cube["cube_url"] + ")\n"
+
+    embed.add_field(name="Cube Name", value=cube_names, inline=True)
+    embed.add_field(name="Cube Alias", value=cube_aliases, inline=True)
+    embed.add_field(name="Cube Owner    Cube Link", value=cube_owners, inline=True)
+
+    await ctx.send(embed=embed)
+          
+@commands.command()
+async def p1p1(ctx, cube_alias: str = ""):
     # get cube data , imageurl, link to cube etc.
-    cube_info = get_cube_data()
+    cube_info = get_cube_data(cube_alias)
 
     cube_message_text= f"Generating a P1P1 from {cube_info["cube_owner_name"]}'s [{cube_info["cube_name"]}]({cube_info["cube_url"]})"
 
@@ -47,6 +73,14 @@ async def p1p1( ctx):
                 await msg.edit(suppress=True)
                 await ctx.send(file=p1p1file)
 
+@commands.command()
+async def cubehelp(ctx):
+    help_message = "I am the Pack1Pick one Bot.\n" \
+                    "Use the below Commands in chat channels:\n" \
+                    "- *** !cubelist *** - Will show the list of cubes to select from (cube_alias shown here)\n" \
+                    "- *** !p1p1 [cube_alias] *** - Will display a random pack1pick1 from either a randomly selected cube or the cube alias provided\n\n" \
+                    "I will also show a daily random Pack1Pick1 in the cube-p1p1 channel at 11am"
+    await ctx.send(help_message)
 
 class P1P1Bot(commands.Bot):
     def __init__(self):
@@ -54,12 +88,14 @@ class P1P1Bot(commands.Bot):
         intents = discord.Intents.default()
         intents.message_content = True
 
-        super().__init__(command_prefix="!", intents=intents)
+        super().__init__(command_prefix="!", intents=intents,help_command=None)
 
     async def setup_hook(self):
         # Start the background task loop when the bot starts up
         self.send_scheduled_message.start()
         self.add_command(p1p1)
+        self.add_command(cubelist)
+        self.add_command(cubehelp)
 
     async def on_ready(self):
         print(f"Logged in as {self.user.name}")
@@ -90,19 +126,31 @@ class P1P1Bot(commands.Bot):
   
 
 # function to get cube data to p1p1 image
-def get_cube_data():
+def get_cube_data(cube_alias=None):
     cube_data = {}
     # get all cube data
     with open('cube_info.json', 'r', encoding='utf-8') as file:
         all_cube_data = json.load(file)
     # randomly select a cube from the list
-    cube_data = random.choice(all_cube_data)
+    cube_alias_lower = cube_alias.lower()
+    if cube_alias == '' or cube_alias_lower == 'random': 
+        cube_data = random.choice(all_cube_data)
+    else:
+        cube_data = next((d for d in all_cube_data if d.get("cube_alias") == cube_alias_lower), None)
     
     # generate random sample pack with seed
     random_seed = random.randint(1111111111111,9999999999999)
     cube_data["cube_image_url"] = f"https://cubecobra.com/cube/samplepackimage/{cube_data["sample_pack_code"]}/{random_seed}"
 
     return cube_data
+
+def get_cube_lsit():
+     # get all cube data
+    with open('cube_info.json', 'r', encoding='utf-8') as file:
+        all_cube_data = json.load(file)
+    
+    return all_cube_data
+
 
 # Run the bot
 if __name__ == '__main__':
